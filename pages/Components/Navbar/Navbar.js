b97efd9b2af1bc928b75323015f7d965c2773e64
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-13 18:06:15 +0500

    initial COmmit

---
+++ pages/Components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import styles from "./Navbar.module.css"
+const Navbar = () => {
+    return ( 
+        <div classname={styles.Navbar_Container}>
+            <div className={styles.logo}>
+                Navbar
+            </div>
+            <div className={styles.menuItems}>
+
+            </div>
+        </div>
+        );
+}
+ 
+export default Navbar;
